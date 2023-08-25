d7bf37482c1f2bfece4e3331862178ec23d26d71
殷亮辉 <yinlianghui@hotoa.com>
2023-08-25 17:20:22 +0800

    set viewport width with space user's zoom setting.

--- services/standard-space/main/default/client/zoom.client.js
+++ services/standard-space/main/default/client/zoom.client.js
@@ -1,8 +1,8 @@
 /*
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-08-22 10:36:38
- * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-08-22 22:19:01
+ * @LastEditors: 殷亮辉 yinlianghui@hotoa.com
+ * @LastEditTime: 2023-08-25 17:18:16
  * @Description: 
  */
 Tracker.autorun(function(e) {
@@ -14,6 +14,21 @@ Tracker.autorun(function(e) {
     if(Steedos.isMobile()){
         $("body").removeClass("zoom-normal").removeClass("zoom-large").removeClass("zoom-extra-large");
         $("body").addClass("zoom-" + zoom);
+        let zoomRate = 1;
+        if(zoom == "large"){
+            zoomRate = 1.25;
+        }else if(zoom == 'extra-large'){
+            zoomRate = 1.35;
+        }
+        if(zoomRate > 1){
+            // var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+            var screenWidth = screen.width;
+            let zoomWidth = screenWidth / zoomRate;
+            document.querySelector("meta[name=viewport]").setAttribute("content", "width=" + zoomWidth + ", user-scalable=0");
+        }
+        else{
+            document.querySelector("meta[name=viewport]").setAttribute("content", "user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width");
+        }
     }else if(Steedos.isNode()){
         let zoomRate = 1;
         if(zoom == "large"){
